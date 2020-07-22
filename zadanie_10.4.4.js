9bcc7817472a896092aaa9f645b53aba76124b76
ytokarevskaya <lan2214@yandex.ru>
2020-07-22 14:42:34 +0300

    Проверено

--- zadanie_10.4.4.js
+++ zadanie_10.4.4.js
@@ -1,5 +1,7 @@
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-getRandomInt(0, 100)
-console.log(getRandomInt(0, 100))
+getRandomInt(0, 101)
+console.log(getRandomInt(0, 101))
+
+// Не совсем верно, т.к. код выше будет возвращать рандомные числа в диапазоне от 0 до 99 (100 включено не будет). Выше исправила на верный вариант
